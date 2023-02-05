b84edb81314f25be000e838393b940b78f01cf11
korotetskiy <93877059+korotetskiy@users.noreply.github.com>
2023-02-05 22:44:51 +0200

    Add files via upload

---
+++ Final project/config.tf
@@ -0,0 +1,62 @@
+provider "aws" {
+  shared_credentials_files = ["/home/vkor/credentials"]
+  region     = "us-east-1"
+}
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_default_vpc" "default" {} # This need to be added since AWS Provider v4.29+ to get VPC id
+
+resource "aws_instance" "my_webserver" {
+  ami                    = "ami-05c5c474dfb6af922"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.my_webserver.id]
+  user_data              = <<EOF
+#!/bin/bash
+yum -y update
+yum -y install httpd
+myip=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
+echo "<h2>WebServer with IP: $myip</h2><br>Build by Terraform!"  >  /var/www/html/index.html
+sudo service httpd start
+chkconfig httpd on
+EOF
+
+  tags = {
+    Name  = "Web Server Build by Terraform"
+    Owner = "Vladimir Korotetskiy"
+  }
+}
+
+
+resource "aws_security_group" "my_webserver" {
+  name        = "WebServer Security Group"
+  description = "My First SecurityGroup"
+  vpc_id      = aws_default_vpc.default.id # This need to be added since AWS Provider v4.29+ to set VPC id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name  = "Web Test SecurityGroup"
+    Owner = "Vladimir Korotetskiy"
+  }
+}
